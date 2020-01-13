3962eaa24f0b8d2aea0d18d2c14c6a1d19b18dac
Bahchaev <wbahas@mail.ru>
2020-01-13 23:37:59 +0700

    added useEffect, useState

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
 //import styles from './styles.module.css';
 
 function Welcome(props) {
@@ -6,15 +7,31 @@ function Welcome(props) {
 }
 
 function App(props) {
-    //const element = <Welcome name="Sara"/>;
+
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 5000);
+    }, []);
+
+    useEffect(() => {
+            console.log(new Date().toLocaleTimeString())
+    },[time]);
+
 
     return (
-        <div>
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
-        </div>
-
+        <>
+            <div>
+                {time}
+            </div>
+            <div>
+                Кнопку нажали {count} раз
+            </div>
+            <button onClick={() => setCount(count + 1)}>Кнопка</button>
+        </>
     )
 }
 
