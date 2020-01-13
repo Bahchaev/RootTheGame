959f6b2bdaa34db36b1b3ad6f6a627b0ea601e68
Bahchaev Alexandr <wbahas@mail.ru>
2020-01-13 17:33:38 +0700

    React start

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import styles from './styles.module.css';
+//import styles from './styles.module.css';
 
-function App() {
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+function App(props) {
+    //const element = <Welcome name="Sara"/>;
+
     return (
-        <div className={styles.App}>
-            adsfasdfasd
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
         </div>
-    );
+
+    )
 }
 
 export default App;
